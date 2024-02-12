a913ed253405c4df464ed0f28235208440f61344
Lenc3lot <noto.baptiste2910@gmail.com>
2024-02-12 17:41:23 +0100

    Maj

--- devapp/src/App.vue
+++ devapp/src/App.vue
@@ -1,18 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav>
+    <img width="50" height="50" src="./assets/logo.png"/>
+    <span>
+      <router-link to="/">Accueil</router-link>
+      <router-link to="/about">Voter</router-link>
+      <router-link to="/login">Connexion</router-link>
+    </span>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -21,6 +17,50 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#app > nav:nth-child(1) {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav>span{
+  /* border: solid 1px;
+  padding: 1%; */
+  width: 30%;
+  display: flex;
+  justify-content: space-between;
+}
+
+nav>span>a{
+  border: solid 1px;
+  border-color: black;
+  width: 100%;
+  height: 100%;
+  padding: 2%;
+}
+
+nav>span>:nth-child(1),:nth-child(2){
+  border-right: none;
+}
+
+nav>span>:nth-child(2),:nth-child(3){
+  border-left: none;
+}
+
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
+
---
+++ devapp/src/views/LoginView.vue
@@ -0,0 +1,52 @@
+<template>
+  <main>
+
+  </main>
+</template>
+
+<script>
+// import store from '@/store'
+export default {
+  name: 'loginView',
+  head () {
+    return {
+      title: 'Document',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Your Page Description'
+        }
+      ]
+    }
+  },
+  components: {
+    // components here
+  },
+  props: {
+    // props here
+  },
+  watch: {
+    // variables to watch here
+  },
+  data () {
+    return {
+      // variables here
+      // dataStore: store
+    }
+  },
+  methods: {
+    // methods here
+  },
+  mounted () {
+    // mounted here
+  },
+  created () {
+    // created here
+  }
+}
+</script>
+
+<style scoped>
+/* Style here */
+</style>
