37457e452bc931997f528bc589b2523f38e73db1
Lenc3lot <noto.baptiste2910@gmail.com>
2024-02-13 17:41:15 +0100

    update

--- devapp/src/App.vue
+++ devapp/src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <nav>
-    <img width="50" height="50" src="./assets/logo.png"/>
-    <span>
-      <router-link to="/">Accueil</router-link>
-      <router-link to="/about">Voter</router-link>
-      <router-link to="/login">Connexion</router-link>
-    </span>
-  </nav>
+  <header class="header">
+		<h1 class="logo">Etu'Connect</h1>
+      <ul class="main-nav">
+          <li><router-link to="/">Accueil</router-link></li>
+          <li><router-link to="/about">Voter</router-link></li>
+          <li><router-link to="/login">Connexion</router-link></li>
+      </ul>
+	</header> 
   <router-view/>
 </template>
 
@@ -19,48 +19,103 @@
   color: #2c3e50;
 }
 
-#app > nav:nth-child(1) {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+* {
+	box-sizing: border-box;
 }
+body {
+	font-family: 'Montserrat', sans-serif;
+	line-height: 1.6;
+	margin: 0;
+	min-height: 100vh;
+  background: linear-gradient(270deg, rgb(103, 151, 221) 0%, white 100%);
 
-nav {
-  padding: 30px;
 }
-
-nav>span{
-  /* border: solid 1px;
-  padding: 1%; */
-  width: 30%;
-  display: flex;
-  justify-content: space-between;
-}
-
-nav>span>a{
-  border: solid 1px;
-  border-color: black;
-  width: 100%;
-  height: 100%;
-  padding: 2%;
-}
-
-nav>span>:nth-child(1),:nth-child(2){
-  border-right: none;
-}
-
-nav>span>:nth-child(2),:nth-child(3){
-  border-left: none;
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
 }
 
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+h2,
+h3,
+a {
+	color: #34495e;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+
+.logo {
+	margin: 0;
+	font-size: 1.45em;
+	padding: 1%;
 }
+
+.main-nav {
+	margin-top: 5px;
+}
+.logo a,
+.main-nav a {
+	padding: 10px 15px;
+	text-transform: uppercase;
+	text-align: center;
+	display: block;
+}
+
+.main-nav a {
+	color: #34495e;
+	font-size: .99em;
+}
+
+.main-nav a:hover {
+	color: #718daa;
+}
+
+
+
+.header {
+	padding-top: .5em;
+	padding-bottom: .5em;
+	border: 1px solid #a2a2a2;
+	background-color: #f4f4f4;
+	-webkit-box-shadow: 0px 0px 14px 0px rgba(0,0,0,0.75);
+	-moz-box-shadow: 0px 0px 14px 0px rgba(0,0,0,0.75);
+	box-shadow: 0px 0px 14px 0px rgba(0,0,0,0.75);
+	-webkit-border-radius: 5px;
+	-moz-border-radius: 5px;
+	border-radius: 5px;
+}
+
+
+/* ================================= 
+  Media Queries
+==================================== */
+
+
+
+
+@media (min-width: 769px) {
+	.header,
+	.main-nav {
+		display: flex;
+	}
+	.header {
+		flex-direction: column;
+		align-items: center;
+		width: 80%;
+		margin: 0 auto;
+		max-width: 100%;
+    margin-top: 1%;
+	}
+
+}
+
+@media (min-width: 1025px) {
+	.header {
+		flex-direction: row;
+		justify-content: space-between;
+	}
+
+}
+
 </style>
 
--- devapp/src/views/LoginView.vue
+++ devapp/src/views/LoginView.vue
@@ -1,13 +1,27 @@
 <template>
-  <main>
-
+  <main id="loginDiv">
+    <div id="saisieDiv">
+      <h2>Espace connexion</h2>
+      <section>
+        <form>
+          <input type="text" placeholder="Login" @change="setLogin($event)">
+          <input type="password" placeholder="Password" @change="setPwd">
+        <figcaption>Merci de vous connecter avec vos identifiants GPI !</figcaption>
+        <input type="submit" class="button-29" value="Se connecter">
+        </form>
+      </section>
+    </div>
+    <div id="logo">
+    <img src="../assets/logoEtu.png" width="750">
+    </div>
   </main>
 </template>
 
 <script>
+/* eslint-disable */
 // import store from '@/store'
 export default {
-  name: 'loginView',
+  name: 'LoginView',
   head () {
     return {
       title: 'Document',
@@ -32,11 +46,19 @@ export default {
   data () {
     return {
       // variables here
-      // dataStore: store
+      // dataStore: store,
+      loginSNS: '',
+      pwd: ''
     }
   },
   methods: {
     // methods here
+    setLogin (event) {
+      this.loginSNS = event.target.value
+    },
+    setPwd (event) {
+      this.pwd = event.target.value
+    }
   },
   mounted () {
     // mounted here
@@ -49,4 +71,117 @@ export default {
 
 <style scoped>
 /* Style here */
+#loginDiv{
+  display: flex;
+  /* border: solid 1px; */
+  align-items: center;
+  text-align: left;
+  height: 41rem;
+}
+
+#loginDiv>:nth-child(1){
+  width: 45%;
+}
+
+#loginDiv>#saisieDiv{
+  display: flex;
+  border: solid 1px;
+  border-radius: 10px;
+  padding: 1%;
+  margin: 1%;
+  background-color: rgb(255, 255, 255);
+  width: 65%;
+  height: 65%;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  box-shadow: 10px 10px 10px black;
+}
+
+#logo{
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+section{
+  width: 65%;
+  display: flex;
+  height: 50%;
+  align-items: center;
+  /* text-align: left; */
+  /* justify-content: center; */
+  flex-direction: column;
+}
+
+img{
+  /* background-color: white; */
+  border-radius: 10px;
+  /* width: 100%; */
+}
+
+section>form>input{
+  margin: 3%;
+  padding: 3%;
+  width: 90%;
+  border-radius: 5px;
+}
+
+.button-29 {
+  margin-top: 2%;
+  align-items: center;
+  appearance: none;
+  background-image: radial-gradient(100% 100% at 100% 0, #5adaff 0, #5468ff 100%);
+  border: 0;
+  border-radius: 6px;
+  box-shadow: rgba(45, 35, 66, .4) 0 2px 4px,rgba(45, 35, 66, .3) 0 7px 13px -3px,rgba(58, 65, 111, .5) 0 -3px 0 inset;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  display: inline-flex;
+  height: 3em;
+  justify-content: center;
+  line-height: 1;
+  list-style: none;
+  overflow: hidden;
+
+  padding-left: 16px;
+  padding-right: 16px;
+  padding: 4%;
+  position: relative;
+  text-align: left;
+  text-decoration: none;
+  transition: box-shadow .15s,transform .15s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  will-change: box-shadow,transform;
+  font-size: 18px;
+}
+
+.button-29:focus {
+  box-shadow: #3c4fe0 0 0 0 1.5px inset, rgba(45, 35, 66, .4) 0 2px 4px, rgba(45, 35, 66, .3) 0 7px 13px -3px, #3c4fe0 0 -3px 0 inset;
+}
+
+.button-29:hover {
+  box-shadow: rgba(45, 35, 66, .4) 0 4px 8px, rgba(45, 35, 66, .3) 0 7px 13px -3px, #3c4fe0 0 -3px 0 inset;
+  transform: translateY(-2px);
+}
+
+.button-29:active {
+  box-shadow: #3c4fe0 0 3px 7px inset;
+  transform: translateY(2px);
+}
+
+figcaption {
+  background-color: #222;
+  color: #fff;
+  font: italic smaller sans-serif;
+  padding: 3px;
+  text-align: center;
+}
 </style>
